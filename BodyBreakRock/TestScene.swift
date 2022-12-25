007ab9e2c1d69f3323903e7e73ab2ccd38221d61
morikentiger <kenblacksword339@gmail.com>
2022-12-25 09:49:39 +0900

    【Swift】SpriteKit Sceneファイルの使い方。ストーリーボードのようにノードをマウスで運んで配置する。(Swift 2.1、XCode 7.2) https://hajihaji-lemon.com/swift/spritekit-scene/

---
+++ BodyBreakRock/TestScene.swift
@@ -0,0 +1,28 @@
+//
+//  TestScene.swift
+//  BodyBreakRock
+//
+//  Created by 森田健太 on 2022/12/25.
+//
+
+import Foundation
+import SpriteKit
+
+class TestScene: SKScene {
+//
+//    override func sceneDidLoad() {
+//
+//        let backNode = SKSpriteNode(imageNamed: "mountain")
+//
+//        backNode.size = self.frame.size
+//
+//        backNode.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame))
+//
+//        let birdNode = SKSpriteNode(imageNamed: "bird1")
+//
+//        birdNode.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame))
+//
+//        self.addChild(backNode)
+//        self.addChild(birdNode)
+//    }
+}
