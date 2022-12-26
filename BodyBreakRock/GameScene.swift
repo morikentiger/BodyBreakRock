a021e83b23cfc700d4bf02f5113244f675d58b6f
morikentiger <kenblacksword339@gmail.com>
2022-12-26 17:47:54 +0900

    余分な記述を消去した。

--- BodyBreakRock/GameScene.swift
+++ BodyBreakRock/GameScene.swift
@@ -33,30 +33,6 @@ class GameScene: SKScene {
     
     var player:AVAudioPlayer!
     
-//    let player = AVQueuePlayer()
-//    //BGM用のパラメーターを作成。
-//    var backgroundMusic = SKAudioNode()
-//    //BGMで流れるゲーム音楽。
-////    let musicURL = Bundle.main.url(forResource: "BecomingTense", withExtension: "m4a")
-//
-//    if let url = Bundle.main.url(forResource: "SomeAudioFile", withExtension: "m4a") {
-//        player.removeAllItems()
-//        player.insert(AVPlayerItem(url: url), after: nil)
-//        player.play()
-//    }
-//    //laserを発射するときの効果音。
-//    let laserSound = SKAction.playSoundFileNamed("LaserSoundEffect.mp3", waitForCompletion: false)
-//
-//    //playerやenemyが爆発したときの効果音。
-//    let explosionSound = SKAction.playSoundFileNamed("explosion.wav", waitForCompletion: false)
-    
-//    var entities = [GKEntity]()
-//    var graphs = [String : GKGraph]()
-//
-//    private var lastUpdateTime : TimeInterval = 0
-//    private var label : SKLabelNode?
-//    private var spinnyNode : SKShapeNode?
-    
     override func sceneDidLoad() {
         
         self.backgroundColor = SKColor.brown
@@ -95,63 +71,16 @@ class GameScene: SKScene {
         } catch {
             print(error)
         }
-        
-//        addChild(SKAudioNode(fileNamed: "BecomingTense.m4a"))
-//        //BGM用の音楽をセットする
-//        backgroundMusic = SKAudioNode(url: musicURL!)
-//        //GameSceneに追加する。
-//        self.addChild(backgroundMusic)
-
-//        self.lastUpdateTime = 0
-//
-//        // Get label node from scene and store it for use later
-//        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-//        if let label = self.label {
-//            label.alpha = 0.0
-//            label.run(SKAction.fadeIn(withDuration: 2.0))
-//        }
-//
-//        // Create shape node to use during mouse interaction
-//        let w = (self.size.width + self.size.height) * 0.05
-//        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-//
-//        if let spinnyNode = self.spinnyNode {
-//            spinnyNode.lineWidth = 2.5
-//
-//            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-//            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-//                                              SKAction.fadeOut(withDuration: 0.5),
-//                                              SKAction.removeFromParent()]))
-//        }
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-        
-        
-//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-//            n.position = pos
-//            n.strokeColor = SKColor.green
-//            self.addChild(n)
-//        }
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        
-        
-//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-//            n.position = pos
-//            n.strokeColor = SKColor.blue
-//            self.addChild(n)
-//        }
     }
     
     func touchUp(atPoint pos : CGPoint) {
-//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-//            n.position = pos
-//            n.strokeColor = SKColor.red
-//            self.addChild(n)
-//        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -179,12 +108,6 @@ class GameScene: SKScene {
             }
             
         }
-        
-//        if let label = self.label {
-//            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-//        }
-//
-//        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -196,11 +119,7 @@ class GameScene: SKScene {
                 let v = CGVector(dx: location.x - base.position.x, dy: location.y - base.position.y)
                 let angle = atan2(v.dy, v.dx)
                 
-//                let deg = angle * CGFloat( 180 / M_PI)
-                //            print( deg + 180)
-                
                 let length: CGFloat = base.frame.size.height / 2
-                //let length: CGFloat = 40
                 
                 let xDist:CGFloat = sin(angle - 1.57079633) * length
                 let yDist:CGFloat = cos(angle - 1.57079633) * length
@@ -211,14 +130,10 @@ class GameScene: SKScene {
                     pad.position = CGPointMake(base.position.x - xDist, base.position.y + yDist)
                 }
                 
-//                darkThunder.zRotation = angle - 1.57079633
                 darkThunder.position.x -= xDist/5
                 darkThunder.position.y += yDist/5
-                
             }
         }
-        
-//        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -246,12 +161,9 @@ class GameScene: SKScene {
         timerJumpButton?.invalidate()
         timerJumpButton = nil
         elapsedTimeJumpButton = 0.0
-        
-//        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
     @objc func updateTimerBreakButton() {
@@ -328,26 +240,7 @@ class GameScene: SKScene {
     }
     
     override func update(_ currentTime: TimeInterval) {
-        
         updateChargeEffect()
-        
         stayOnScreen()
-        
-//        // Called before each frame is rendered
-//
-//        // Initialize _lastUpdateTime if it has not already been
-//        if (self.lastUpdateTime == 0) {
-//            self.lastUpdateTime = currentTime
-//        }
-//
-//        // Calculate time since last update
-//        let dt = currentTime - self.lastUpdateTime
-//
-//        // Update entities
-//        for entity in self.entities {
-//            entity.update(deltaTime: dt)
-//        }
-//
-//        self.lastUpdateTime = currentTime
     }
 }
