aa93d623305294b31363dac16013bd37324ae775
morikentiger <kenblacksword339@gmail.com>
2022-12-26 12:54:43 +0900

    バーチャルジョイスティックの位置を調節した。ジャンプボタン、ブレイクボタンを配置した。

--- BodyBreakRock/GameScene.swift
+++ BodyBreakRock/GameScene.swift
@@ -7,14 +7,36 @@
 
 import SpriteKit
 import GameplayKit
+import AVFoundation
 
 class GameScene: SKScene {
     
     let base = SKSpriteNode(imageNamed: "JoystickBase")
     let pad = SKSpriteNode(imageNamed: "JoystickPad")
+    let jumpButton = SKSpriteNode(imageNamed: "JumpButton")
+    let breakButton = SKSpriteNode(imageNamed: "BreakButton")
     let darkThunder = SKSpriteNode(imageNamed: "DarkThunder")
     
     var sticActive: Bool = false
+    
+    var player:AVAudioPlayer!
+    
+//    let player = AVQueuePlayer()
+//    //BGM用のパラメーターを作成。
+//    var backgroundMusic = SKAudioNode()
+//    //BGMで流れるゲーム音楽。
+////    let musicURL = Bundle.main.url(forResource: "BecomingTense", withExtension: "m4a")
+//
+//    if let url = Bundle.main.url(forResource: "SomeAudioFile", withExtension: "m4a") {
+//        player.removeAllItems()
+//        player.insert(AVPlayerItem(url: url), after: nil)
+//        player.play()
+//    }
+//    //laserを発射するときの効果音。
+//    let laserSound = SKAction.playSoundFileNamed("LaserSoundEffect.mp3", waitForCompletion: false)
+//
+//    //playerやenemyが爆発したときの効果音。
+//    let explosionSound = SKAction.playSoundFileNamed("explosion.wav", waitForCompletion: false)
     
 //    var entities = [GKEntity]()
 //    var graphs = [String : GKGraph]()
@@ -30,16 +52,43 @@ class GameScene: SKScene {
         self.anchorPoint = CGPointMake(0.5, 0.5)
         
         self.addChild(base)
-        base.position = CGPointMake(0, -200)
+        base.position = CGPointMake(-100, -250)
         
         self.addChild(pad)
         pad.position = base.position
+        
+        self.addChild(jumpButton)
+        jumpButton.position = CGPointMake(75, -275)
+        
+        self.addChild(breakButton)
+        breakButton.position = CGPointMake(125, -225)
         
         self.addChild(darkThunder)
         darkThunder.position = CGPointMake(0, 200)
         
         pad.alpha = 0.4
         base.alpha = 0.4
+        jumpButton.alpha = 0.4
+        breakButton.alpha = 0.4
+        
+        let url = Bundle.main.bundleURL.appendingPathComponent("BecomingTense.mp3")
+        do {
+            try player = AVAudioPlayer(contentsOf:url)
+            
+            //音楽を再生する。
+            player.play()
+            //ループさせる
+            player.numberOfLoops = -1
+            
+        } catch {
+            print(error)
+        }
+        
+//        addChild(SKAudioNode(fileNamed: "BecomingTense.m4a"))
+//        //BGM用の音楽をセットする
+//        backgroundMusic = SKAudioNode(url: musicURL!)
+//        //GameSceneに追加する。
+//        self.addChild(backgroundMusic)
 
 //        self.lastUpdateTime = 0
 //
@@ -134,7 +183,9 @@ class GameScene: SKScene {
                     pad.position = CGPointMake(base.position.x - xDist, base.position.y + yDist)
                 }
                 
-                darkThunder.zRotation = angle - 1.57079633
+//                darkThunder.zRotation = angle - 1.57079633
+                darkThunder.position.x -= xDist/10
+                darkThunder.position.y += yDist/10
                 
             }
         }
