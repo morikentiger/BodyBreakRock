eff924a25f695f57354104b86db08cfa5c7e7824
morikentiger <kenblacksword339@gmail.com>
2022-12-27 16:14:10 +0900

    緊迫感のあるステージ落下系ステージができてしまった（当たり判定・・・）

--- BodyBreakRock/GameScene.swift
+++ BodyBreakRock/GameScene.swift
@@ -9,15 +9,49 @@ import SpriteKit
 import GameplayKit
 import AVFoundation
 
-class GameScene: SKScene {
+//class BlockNode: SKSpriteNode {
+//
+//}
+//
+//extension BlockNode {
+//    static let category: UInt32 = 0x1 << 0
+//}
+//
+//class PlayerNode: SKSpriteNode {
+//
+//}
+//
+//extension PlayerNode {
+//    static let category: UInt32 = 0x1 << 1
+//}
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     let base = SKSpriteNode(imageNamed: "JoystickBase")
     let pad = SKSpriteNode(imageNamed: "JoystickPad")
     let jumpButton = SKSpriteNode(imageNamed: "JumpButton")
     let breakButton = SKSpriteNode(imageNamed: "BreakButton")
+    
+    let cursor = SKSpriteNode(color: .red, size: CGSize(width: 50, height: 50))
+    
     let darkThunder = SKSpriteNode(imageNamed: "DarkThunderModeStanding")
     
-    var sticActive: Bool = false
+    var blocks: [SKSpriteNode] = []
+    
+    let stage = SKNode()
+
+//    let block = SKSpriteNode(imageNamed: "block")
+//                 SKSpriteNode(imageNamed: "block"),
+//                 SKSpriteNode(imageNamed: "block"),
+//                 SKSpriteNode(imageNamed: "block"),
+//                 SKSpriteNode(imageNamed: "block"),
+//                 SKSpriteNode(imageNamed: "block"),
+//                 SKSpriteNode(imageNamed: "block"),
+//                 SKSpriteNode(imageNamed: "block"),
+//                 SKSpriteNode(imageNamed: "block"),
+//                 SKSpriteNode(imageNamed: "block")]
+    
+    var isSticActive = false
     var isBreakButtonPressed = false
     var isJumpButtonPressed = false
     
@@ -33,11 +67,51 @@ class GameScene: SKScene {
     
     var player:AVAudioPlayer!
     
+    let labelIsBreakButtonPressed = SKLabelNode(fontNamed: "Chalkduster")
+    
     override func sceneDidLoad() {
+        
+        labelIsBreakButtonPressed.text = "nil"
+        labelIsBreakButtonPressed.fontSize = 39
+        labelIsBreakButtonPressed.fontColor = SKColor.red
+        labelIsBreakButtonPressed.position = CGPoint(x: 0 ,y: 0)
+        addChild(labelIsBreakButtonPressed)
+        
+        self.physicsWorld.contactDelegate = self
+        
         
         self.backgroundColor = SKColor.brown
         
         self.anchorPoint = CGPointMake(0.5, 0.5)
+        
+        
+//        for i in 0...9 {
+//            self.addChild(block[i])
+//            block[i].position = CGPointMake(0, CGFloat(-500+i*50))
+//        }
+        for x in 0..<10 {
+            for y in 0..<10 {
+                let block = SKSpriteNode(imageNamed: "block")
+                block.position = CGPoint(x: -250+x*50, y: -250+y*50)
+                blocks.append(block)
+                stage.addChild(block)
+                
+//                let blockCopy = block.copy() as! SKSpriteNode
+//                blockCopy.position = CGPoint(x: -250+x*50, y: -250+y*50)
+                
+//                // ブロックに物理演算を有効にする
+//                blockCopy.physicsBody = SKPhysicsBody(rectangleOf: blockCopy.size)
+//                blockCopy.physicsBody?.isDynamic = false // 静的なオブジェクトにする
+//
+//                // ブロックと衝突したときに通知を受け取るカテゴリーを設定する
+//                blockCopy.physicsBody?.categoryBitMask = 0b0001 // 0001
+//                blockCopy.physicsBody?.contactTestBitMask = 0b0010 // 0010
+                
+//                stage.addChild(blockCopy)
+            }
+        }
+        
+        self.addChild(stage)
         
         self.addChild(base)
         base.position = CGPointMake(-100, -250)
@@ -51,6 +125,10 @@ class GameScene: SKScene {
         self.addChild(breakButton)
         breakButton.position = CGPointMake(125, -225)
         
+        self.addChild(cursor)
+        cursor.isHidden = true
+        
+        
         self.addChild(darkThunder)
         darkThunder.position = CGPointMake(0, 200)
         
@@ -58,6 +136,22 @@ class GameScene: SKScene {
         base.alpha = 0.4
         jumpButton.alpha = 0.4
         breakButton.alpha = 0.4
+        
+        // 衝突時に呼ばれるデリゲートメソッドを設定する
+        self.physicsWorld.contactDelegate = self
+//        for i in 0...9 {
+        
+//        }
+        // プレイヤーに物理演算を有効にする
+        darkThunder.physicsBody = SKPhysicsBody(rectangleOf: darkThunder.size)
+        darkThunder.physicsBody?.mass = 0.0
+        darkThunder.physicsBody?.affectedByGravity = false // 重力を適用しない
+
+
+        // プレイヤーと衝突したときに通知を受け取るカテゴリーを設定する
+        darkThunder.physicsBody?.collisionBitMask = 0b0100 // 0100
+        darkThunder.physicsBody?.categoryBitMask = 0b0010 // 0010
+        darkThunder.physicsBody?.contactTestBitMask = 0b0001 // 0001
         
         let url = Bundle.main.bundleURL.appendingPathComponent("BecomingTense.mp3")
         do {
@@ -88,23 +182,27 @@ class GameScene: SKScene {
             let location = touch.location(in: self)
             
             if (CGRectContainsPoint(pad.frame, location)) {
-                sticActive = true
+                isSticActive = true
+                
             } else {
-                sticActive = false
+                isSticActive = false
             }
             
             if (CGRectContainsPoint(breakButton.frame, location)) {
                 isBreakButtonPressed = true
                 timerBreakButton = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateTimerBreakButton), userInfo: nil, repeats: true)
+                cursor.isHidden = false
+                cursor.position.x = darkThunder.position.x + 50
+                cursor.position.y = darkThunder.position.y + 50
             } else {
-                isBreakButtonPressed = false
+                
             }
             
             if (CGRectContainsPoint(jumpButton.frame, location)) {
                 isJumpButtonPressed = true
                 timerJumpButton = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateTimerJumpButton), userInfo: nil, repeats: true)
             } else {
-                isJumpButtonPressed = false
+                
             }
             
         }
@@ -114,7 +212,7 @@ class GameScene: SKScene {
         for touch in (touches ) {
             let location = touch.location(in: self)
             
-            if (sticActive == true) {
+            if (isSticActive == true) {
                 
                 let v = CGVector(dx: location.x - base.position.x, dy: location.y - base.position.y)
                 let angle = atan2(v.dy, v.dx)
@@ -129,35 +227,49 @@ class GameScene: SKScene {
                 } else {
                     pad.position = CGPointMake(base.position.x - xDist, base.position.y + yDist)
                 }
-                
-                darkThunder.position.x -= xDist/5
-                darkThunder.position.y += yDist/5
+                if !isBreakButtonPressed {
+                    darkThunder.position.x -= xDist/5
+                    darkThunder.position.y += yDist/5
+                }
+                if isBreakButtonPressed {
+                    cursor.position.x -= xDist/5
+                    cursor.position.y += yDist/5
+                }
             }
         }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if (sticActive == true) {
-            let move:SKAction = SKAction.move(to: base.position, duration: 0.2)
-            move.timingMode = .easeOut
+        if isSticActive {
+            let movePad:SKAction = SKAction.move(to: base.position, duration: 0.2)
+            movePad.timingMode = .easeOut
             
-            pad.run(move)
+            pad.run(movePad)
         }
         
         darkThunder.removeAllChildren()
         
         if isBreakButtonPressed {
-            darkThunder.position.x += moveBreakButton
+//            darkThunder.position.x += moveBreakButton
+            let moveBreak:SKAction = SKAction.move(to: CGPoint(x: cursor.position.x, y: cursor.position.y), duration: 0.2)
+            moveBreak.timingMode = .easeOut
+            
+            darkThunder.run(moveBreak)
+            
+            isBreakButtonPressed = false
+            cursor.isHidden = true
+            
         }
-        isBreakButtonPressed = false
+        
         timerBreakButton?.invalidate()
         timerBreakButton = nil
         elapsedTimeBreakButton = 0.0
         
         if isJumpButtonPressed {
             darkThunder.position.y += moveJumpButton
+            isJumpButtonPressed = false
         }
-        isJumpButtonPressed = false
+        
         timerJumpButton?.invalidate()
         timerJumpButton = nil
         elapsedTimeJumpButton = 0.0
@@ -239,8 +351,74 @@ class GameScene: SKScene {
         }
     }
     
+    override func didSimulatePhysics() {
+        for block in blocks {
+            block.physicsBody = SKPhysicsBody(rectangleOf: block.size)
+            block.physicsBody?.categoryBitMask = darkThunder.physicsBody!.categoryBitMask
+            block.physicsBody?.collisionBitMask = darkThunder.physicsBody!.collisionBitMask
+            block.physicsBody?.contactTestBitMask = darkThunder.physicsBody!.contactTestBitMask
+        }
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        
+        for block in blocks {
+            let bodyA = contact.bodyA
+            let bodyB = contact.bodyB
+            
+            if bodyA.categoryBitMask == darkThunder.physicsBody?.categoryBitMask && bodyB.categoryBitMask == block.physicsBody?.categoryBitMask || bodyA.categoryBitMask == block.physicsBody?.categoryBitMask && bodyB.categoryBitMask == darkThunder.physicsBody?.categoryBitMask {
+                block.removeFromParent()
+            }
+        }
+        // 衝突が発生したときに呼ばれるメソッド
+//        // 衝突したオブジェクトを取得する
+//        let objectA = contact.bodyA.node
+//        let objectB = contact.bodyB.node
+//
+//        // ブロックとプレイヤーが衝突した場合
+//        for i in 0...9 {
+//            if (objectA == stage.childNode(withName: "blockCopy") && objectB == darkThunder) || (objectA == darkThunder && objectB == stage.childNode(withName: "blockCopy")) {
+//                // ブロックを削除する
+//                stage.childNode(withName: "blockCopy")?.removeFromParent()
+//            }
+//        }
+        
+//        if let playerNode = nodeA as? PlayerNode, let blockNode = nodeB as? BlockNode {
+////            playerNode.physicsBody = SKPhysicsBody(rectangleOf: playerNode.frame.width)
+//            playerNode.physicsBody?.contactTestBitMask = BlockNode.category
+//        } else if let playerNode = nodeB as? PlayerNode, let blockNode = nodeA as? BlockNode {
+//            playerNode.physicsBody?.contactTestBitMask = BlockNode.category
+//            self.physicsWorld.contactTestBitMask = BlockNode.category
+//            blockNode.physicsBody = nil
+//        }
+    }
+    
+    func changeButtonAlpha() {
+        if isSticActive {
+            pad.alpha = 1.0
+        } else {
+            pad.alpha = 0.4
+        }
+        
+        if isBreakButtonPressed {
+            breakButton.alpha = 1.0
+        } else {
+            breakButton.alpha = 0.4
+        }
+        
+        if isJumpButtonPressed {
+            jumpButton.alpha = 1.0
+        } else {
+            jumpButton.alpha = 0.4
+        }
+    }
+    
     override func update(_ currentTime: TimeInterval) {
+        changeButtonAlpha()
         updateChargeEffect()
         stayOnScreen()
+        
+        labelIsBreakButtonPressed.text = "\(isBreakButtonPressed)"
     }
 }
+
